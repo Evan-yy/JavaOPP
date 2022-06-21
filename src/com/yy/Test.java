14b3de167e3ebfec9961b7b31a0a9c6ce604107e
Evan-yy <jjjklj@qq.com>
2022-06-21 18:26:43 +0800

    first

---
+++ src/com/yy/Test.java
@@ -0,0 +1,8 @@
+package com.yy;
+
+public class Test {
+  public static void main(String[] args) {
+        Student A = new Student("Evan",22,"男",123456);
+      System.out.println(A.toString());
+    }
+}
